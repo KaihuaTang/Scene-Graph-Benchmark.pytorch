1b88521f7b2182c967132eed1add859cd86d917c
yizhiyua <gyqjz@mail.nwpu.edu.cn>
2020-03-17 20:12:08 +0800

    3.17

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -106,13 +106,13 @@ class DatasetCatalog(object):
             "ann_file": "cityscapes/annotations/instancesonly_filtered_gtFine_test.json"
         },
         "VG_stanford_filtered": {
-            "img_dir": "vg/VG_100K",
+            "img_dir": "vg/images",
             "roidb_file": "vg/VG-SGG.h5",
             "dict_file": "vg/VG-SGG-dicts.json",
             "image_file": "vg/image_data.json",
         },
         "VG_stanford_filtered_with_attribute": {
-            "img_dir": "vg/VG_100K",
+            "img_dir": "vg/images",
             "roidb_file": "vg/VG-SGG-with-attri.h5",
             "dict_file": "vg/VG-SGG-dicts-with-attri.json",
             "image_file": "vg/image_data.json",
